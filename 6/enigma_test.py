8c93478d020d62340e66b119cee2a39a9491cfbe
Aliaksandr.Valai <valexxjr@gmail.com>
2025-06-15 16:00:56 +0200

    added coverage

--- 6/enigma_test.py
+++ 6/enigma_test.py
@@ -1,5 +1,10 @@
 import unittest
-from enigma import Enigma
+import coverage
+from enigma import Enigma, Rotor, plugboard_swap, mod
+
+# Start coverage measurement
+cov = coverage.Coverage()
+cov.start()
 
 class TestEnigma(unittest.TestCase):
     def test_basic_encryption_decryption(self):
@@ -22,5 +27,36 @@ class TestEnigma(unittest.TestCase):
         encrypted = enigma.process(message)
         self.assertEqual(len(message), len(encrypted))
 
+    def test_rotor_stepping(self):
+        rotor = Rotor('EKMFLGDQVZNTOWYHXUSPAIBRCJ', 'Q')
+        initial_pos = rotor.position
+        rotor.step()
+        self.assertEqual(rotor.position, mod(initial_pos + 1, 26))
+
+    def test_rotor_at_notch(self):
+        rotor = Rotor('EKMFLGDQVZNTOWYHXUSPAIBRCJ', 'Q', position=16)  # Q is at position 16
+        self.assertTrue(rotor.at_notch())
+
+    def test_plugboard_swap_function(self):
+        pairs = [['A', 'B'], ['C', 'D']]
+        self.assertEqual(plugboard_swap('A', pairs), 'B')
+        self.assertEqual(plugboard_swap('B', pairs), 'A')
+        self.assertEqual(plugboard_swap('C', pairs), 'D')
+        self.assertEqual(plugboard_swap('D', pairs), 'C')
+        self.assertEqual(plugboard_swap('E', pairs), 'E')  # No swap
+
+    def test_non_alphabetic_characters(self):
+        enigma = Enigma([0, 1, 2], [0, 0, 0], [0, 0, 0], [])
+        message = 'HELLO 123!'
+        encrypted = enigma.process(message)
+        self.assertEqual(len(message), len(encrypted))
+        self.assertEqual(encrypted[5:], ' 123!')  # Non-alphabetic chars should be preserved
+
 if __name__ == '__main__':
-    unittest.main() 
+    unittest.main()
+    
+    # Stop coverage measurement and generate report
+    cov.stop()
+    cov.save()
+    print('\nCoverage Report:')
+    cov.report() 
